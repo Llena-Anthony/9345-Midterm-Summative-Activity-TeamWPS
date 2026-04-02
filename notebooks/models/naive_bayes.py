082c04922eb708e7954e15ad81fdd446479b3ab5
Nathaniel de la Rosa <2230188@slu.edu.ph>
2026-04-02 20:23:36 +0800

    re-polished naive_bayes, added documentation

--- notebooks/models/naive_bayes.py
+++ notebooks/models/naive_bayes.py
@@ -1,13 +1,33 @@
+# # ======================================
+# Programmer: Nathaniel de la Rosa
+# Date Programmed: April 02, 2026
+
+# Objectives:
+# Model 2 – Naive Bayes
+# Responsible for implementing a probabilistic classification model.
+
+# Tasks performed:
+# Load the processed dataset
+# Split data into training, validation, and test sets
+# Train a Gaussian Naive Bayes model
+# Generate predictions on validation and test sets
+# Evaluate model performance using classification report
+# Save results to outputs/naive_bayes_results.txt
+
+# Outputs:
+# outputs/naive_bayes_results.txt
+# # ======================================
+
 from pathlib import Path
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
-from sklearn.metrics import classification_report, accuracy_score
+from sklearn.metrics import classification_report
 
 # Paths
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 DATA_PATH = BASE_DIR / "data" / "processed" / "cleaned_stroke_data.csv"
-OUTPUT_DIR = BASE_DIR / "notebooks" / "outputs"
+OUTPUT_DIR = BASE_DIR / "notebooks" / "outputs" # I'll modify once the folder structure is finalized
 
 # Load dataset
 df = pd.read_csv(DATA_PATH)
@@ -19,10 +39,10 @@ y = df["stroke"]
 # Note: I think train-test-val is supposed to be the same across all the models.
 # Remove this code block and replace with calls to folders containing the split data.
 # 1. First split: 90% train+val, 10% test
-X_train_val, X_test, y_train_val, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
+X_train_val, X_test, y_train_val, y_test = train_test_split(X, y, test_size=0.1, random_state=42, stratify=y)
 
 # 2. Second split: 80% of X_train_val goes to train, 20% to validation
-X_train, X_val, y_train, y_val = train_test_split(X_train_val, y_train_val, test_size=0.2, random_state=42)
+X_train, X_val, y_train, y_val = train_test_split(X_train_val, y_train_val, test_size=0.2, random_state=42, stratify=y_train_val)
 
 
 # Train model
